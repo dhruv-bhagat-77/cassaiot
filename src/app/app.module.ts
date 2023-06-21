833c929b8cf1597c260c19171587cf5d032e6882
Dhruv Bhagat <dhruv.bhagat.1256@gmail.com>
2023-06-22 01:24:25 +0530

    added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { FooterComponent } from './footer/footer.component';
 // import {coreModule} from "./core/core";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
